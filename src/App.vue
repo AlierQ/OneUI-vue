145a738a4487fb439d2dfd4887865df6f957d9d6
AlierQ <1029369938@qq.com>
2022-07-15 10:41:24 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MyComponent></MyComponent>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent
+  }
+}
+</script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我的组件
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MyComponent'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
