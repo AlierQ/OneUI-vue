fd55c755d2acdea253a0e56f166fc63194e22ec7
AlierQ <1029369938@qq.com>
2022-07-18 23:43:28 +0800

    add restrict: Tabs can only put Tab inside

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,9 +4,21 @@
 
 <script lang="ts">
 import Tab from './Tab.vue';
+
 export default {
-  name: 'Tabs',
-  components: {Tab}
+  components: {Tab},
+  setup(props, context) {
+    // console.log({...context});
+    // context.slots.default() 存放的时Tabs内部放的所有组件
+    const defaults = context.slots.default()
+    // 通过其身上的.type属性就能获取到该组件信息，从而可以判断使用的是什么标签或者组件
+    defaults.forEach((tag)=>{
+      // 如果Tabs中不是Tab就报错
+      if (tag.type !== Tab){
+        throw new Error('Tabs can only put Tab inside')
+      }
+    })
+  }
 };
 </script>
 
