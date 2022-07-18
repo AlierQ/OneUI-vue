0036666bb8d79aea2f8234feea3e09b3ddab6ad9
AlierQ <1029369938@qq.com>
2022-07-18 22:27:10 +0800

    create component tabs tab and write html

--- src/components/TabsPage.vue
+++ src/components/TabsPage.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>Tabs组件</div>
+  <div>
+    <h3>Tabs 标签页</h3>
+    <div>
+      <Tabs>
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+        <Tab title="导航3">内容3</Tab>
+      </Tabs>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import Tabs from '../lib/Tabs.vue';
+import Tab from '../lib/Tab.vue';
+
 export default {
-  name: 'Tabs'
+  components: {
+    Tabs,
+    Tab
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 div {
   width: 100%;
-  background: yellow;
+  //background: yellow;
   overflow: auto;
 }
 </style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>tabs组件</div>
+</template>
+
+<script lang="ts">
+import Tab from './Tab.vue';
+export default {
+  name: 'Tabs',
+  components: {Tab}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
