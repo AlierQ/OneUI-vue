fb034a23c148ed6be993824c1dc21cca986b4cc6
AlierQ <1029369938@qq.com>
2022-07-18 15:37:51 +0800

    write model html

--- src/lib/Model.vue
+++ src/lib/Model.vue
@@ -1,13 +1,23 @@
 <template>
-  <div></div>
+  <div class="one-model-shade"></div>
+  <div class="one-model">
+    <header>这里是标题</header>
+    <main>这里是内容</main>
+    <footer>
+      <Button type="text">取消</Button>
+      <Button type="primary">确定</Button>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
+import Button from './Button.vue';
 export default {
-  name: 'Model'
+  name: 'Model',
+  components: {Button}
 };
 </script>
 
 <style lang="scss" scoped>
-
+//rgba(55,55,55,.6);
 </style>
