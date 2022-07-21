8f53b458556f4ee66884954981d328bc9bdee005
AlierQ <1029369938@qq.com>
2022-07-21 10:22:07 +0800

    global use Markdown

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,11 +1,3 @@
 <template>
   <Markdown path="../markdown/install.md"></Markdown>
 </template>
-
-<script lang="ts">
-import Markdown from '../components/Markdown.vue';
-
-export default {
-  components: {Markdown},
-};
-</script>
--- src/views/Started.vue
+++ src/views/Started.vue
@@ -1,11 +1,3 @@
 <template>
   <Markdown path="../markdown/started.md"></Markdown>
 </template>
-
-<script lang="ts">
-import Markdown from '../components/Markdown.vue';
-
-export default {
-  components: {Markdown},
-};
-</script>
