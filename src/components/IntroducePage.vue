fd3b5dbbc4690c2d46d2c5c0185f39c600d14c8b
AlierQ <1029369938@qq.com>
2022-07-24 16:44:20 +0800

    update introduce

--- src/components/IntroducePage.vue
+++ src/components/IntroducePage.vue
@@ -6,13 +6,14 @@
 
     <p class="indent"><strong>One UI</strong> 是一款简单易用的开源前端UI组件，它基于 <strong>Vue3</strong> 进行开发，使用
       <strong>TypeScript</strong> 进行编写，并使用 <strong>Vite3</strong> 进行项目构建和打包。
-      One UI使用了当下流行的扁平化设计使得组件的风格简洁、美观。此外One UI 的使用使用方式也是极为的简单、便捷。</p>
+      <strong>One UI</strong> 使用了当下流行的扁平化设计使得组件的风格简洁、美观。此外 <strong>One UI</strong> 的使用方式也是极为的简单、便捷。</p>
 
     <p class="indent"><strong>One UI</strong> 在编写的过程中尽可能的不使用第三方库，几乎所有组件均为手写开发。此外，<strong>One UI</strong>
-      组件在编写中一直秉承了简洁的原则，所有的组件源码都足够简洁易懂。并且我会在此文档中展示组件的使用效果以及示例代码，希望对你的开发有所帮助</p>
+      组件在编写中一直秉承了简洁的原则，所有的组件源码都足够简洁易懂。并且我会在此文档中展示组件的使用效果以及示例代码，希望对你的开发有所帮助。</p>
 
-    <p>如果你对 <strong>One UI</strong> 感兴趣，请移步至 Github <a href="https://github.com/AlierQ/OneUI-vue" class="link">[传送门]</a>。当然，也可以欢迎star
-    </p>
+    <p class="indent">如果你对 <strong>One UI</strong> 的源码感兴趣，请移步至 <strong>Github</strong> <a href="https://github.com/AlierQ/OneUI-vue" class="link">[传送门]</a>。当然，也欢迎 <strong>star⭐</strong></p>
+
+    <p class="indent">关于本项目的遇到的相关问题以及解决方式，我会放在我的博客中，如果你感兴趣你也可以移步至我的博客 <strong>AlierQ's Blog</strong> <a href="https://alierq.space/" class="link">[传送门]</a> 。</p>
   </div>
 </template>
 
@@ -47,10 +48,11 @@ p {
   font-size: 16px;
   line-height: 1.8;
   margin: 10px 0;
+  word-break:break-all;
 }
 
 .indent {
-  text-indent: 20px;
+  text-indent: 32px;
 }
 
 .link {
