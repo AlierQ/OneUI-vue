aaeb617f1266351eaedad04d4ba8efc3b4dca7a7
AlierQ <1029369938@qq.com>
2022-07-24 18:06:42 +0800

    use svg in MenuTitle

--- src/lib/Menu/MenuTitle.vue
+++ src/lib/Menu/MenuTitle.vue
@@ -5,15 +5,16 @@
     </div>
     <div class="one-menutitle-icon"
          :class="{toggle:isOpen}">
-      <svg class="iconpark-icon">
-        <use href="#down"></use>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-width="4" stroke="#515a6e" d="M36 18 24 30 12 18"
+              data-follow-stroke="#515a6e"/>
       </svg>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {inject, onMounted, Ref, ref} from 'vue';
+import {inject, Ref, ref} from 'vue';
 
 export default {
   props: {
